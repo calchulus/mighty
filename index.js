5cc63ad8baf114433da67c83edda68a1e1f767c0
Calvin Chu <calc@uchicago.edu>
2018-04-09 10:05:01 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,41 +8,40 @@
     messagingSenderId: "475267345622"
   };
   firebase.initializeApp(config);
-
 $('.ui.dropdown')
   .dropdown();
+
 $('.ui.radio.checkbox')
   .checkbox();
 
 // New Firebase
-var resourceDB = firebase.database().ref('userInfo');
-var userDB = firebase.database().ref('users');
-var first =  
+var resourceDB = firebase.database().ref('resource-tip');
+var userDB = firebase.database().ref('userInfo');
+//var userDB = firebase.database().ref('users');
   // Add a New Item
   $("#userInfo").submit(function(event) {
-      event.preventDefault();
+//      event.preventDefault();
     // Get the form data
       console.log(document.getElementById('firstname').value)
-    resourceDB.push({
-
+      console.log("hello")
+    userDB.push({
         'firstname': $('#firstname').val(),
         'lastname': $('#lastname').val(),
-////        document.getElementById('firstname').value,
-//        'lastname': document.getElementById('lastname').value,
-//        'email': document.getElementById('email').value,
-//        'bank': document.getElementById('bank').value,
-//        'gender':document.getElementById('gender').value,
-//           'state':document.getElementById('state').value,
-//         'zipcode': document.getElementById('zipcode').value,
-//        'frequency': document.getElementById('frequency').value,
-//      'causes': document.getElementById('causes').value,
-//        'comments': document.getElementById('comments').value
+        'lastname': document.getElementById('lastname').val(),
+        'email': document.getElementById('email').val(),
+        'bank': document.getElementById('bank').val(),
+        'gender':document.getElementById('gender').val(),
+           'state':document.getElementById('state').val(),
+         'zipcode': document.getElementById('zipcode').val(),
+        'frequency': document.getElementById('frequency').val(),
+      'causes': document.getElementById('causes').val(),
+        'comments': document.getElementById('comments').val(),
             'tipTime': Date.now()
     }); 
     console.log("sent");
-    resourceformTip.reset();
-      resourceDB = firebase.database().ref('userInfo');
-      resourceRef = resourceDB.orderByChild('tipTime');
+    userInfo.reset();
+//      resourceDB = firebase.database().ref('userInfo');
+//      resourceRef = resourceDB.orderByChild('tipTime');
 
   });
 
