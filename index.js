9d1bb7db853f7b36ffde3ea0da3de816cc20bd54
Calvin Chu <calc@uchicago.edu>
2018-04-09 19:25:57 -0500

    working version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyB2MVaewZFZQFZBDuf1ab2zeH8kxRwS6vs",
     authDomain: "mightydeposits.firebaseapp.com",
@@ -10,9 +10,8 @@
   firebase.initializeApp(config);
 $('.ui.dropdown')
   .dropdown();
-
-$('.ui.radio.checkbox')
-  .checkbox();
+//$('.ui.radio.checkbox')
+//  .checkbox();
 
 // New Firebase
 var resourceDB = firebase.database().ref('resource-tip');
@@ -20,29 +19,25 @@ var userDB = firebase.database().ref('userInfo');
 //var userDB = firebase.database().ref('users');
   // Add a New Item
   $("#userInfo").submit(function(event) {
-//      event.preventDefault();
+      event.preventDefault();
     // Get the form data
-      console.log(document.getElementById('firstname').value)
       console.log("hello")
     userDB.push({
-        'firstname': $('#firstname').val(),
-        'lastname': $('#lastname').val(),
-        'lastname': document.getElementById('lastname').val(),
-        'email': document.getElementById('email').val(),
-        'bank': document.getElementById('bank').val(),
-        'gender':document.getElementById('gender').val(),
-           'state':document.getElementById('state').val(),
-         'zipcode': document.getElementById('zipcode').val(),
-        'frequency': document.getElementById('frequency').val(),
-      'causes': document.getElementById('causes').val(),
-        'comments': document.getElementById('comments').val(),
+//        'firstname': $('#firstname').val(),
+       'firstname': document.getElementById('firstname').value,
+        'lastname': document.getElementById('lastname').value,
+        'email': document.getElementById('email').value,
+        'bank': document.getElementById('bank').value,
+        'gender':document.getElementById('gender').value,
+           'state':document.getElementById('state').value,
+         'zipcode': document.getElementById('zipcode').value,
+        'frequency': document.getElementById('frequency').value,
+      'causes': document.getElementById('causes').value,
+        'comments': document.getElementById('comments').value,
             'tipTime': Date.now()
     }); 
     console.log("sent");
     userInfo.reset();
-//      resourceDB = firebase.database().ref('userInfo');
-//      resourceRef = resourceDB.orderByChild('tipTime');
-
   });
 
 
